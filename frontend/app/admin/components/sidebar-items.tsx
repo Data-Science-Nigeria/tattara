75f66602073d26ff940b6809506a9d172b752b62
BalkyBoy <bolugafar360@gmail.com>
2025-09-08 13:53:46 +0100

    feat: created next app

--- frontend/app/admin/components/sidebar-items.tsx
+++ frontend/app/admin/components/sidebar-items.tsx
@@ -11,7 +11,7 @@ export const sidebarItems = [
     name: 'Profile',
     href: '/admin/profile',
     icon: User,
-    alt: 'Profile icon',
+    alt: 'Profile ion',
   },
   {
     name: 'Create Workflow',
